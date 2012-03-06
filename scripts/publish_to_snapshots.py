5b86cdf17ace0b77d2d314e795a8bfdda9edb21d
Deepti B. Kalakeri <deepti.kalakeri@linaro.org>
2012-03-06 17:18:23 +0530

    Adding the command line parsing

--- scripts/publish_to_snapshots.py
+++ scripts/publish_to_snapshots.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
-import optparse
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-j", "--job_type", dest="job_type",
+                    help="Specifiy the job type (Ex: android/kernel-hwpack)")
+parser.add_argument("-a", "--archive_info", dest="archive_info",
+                    help="Specify the job which resulted the archive to be stored.\
+                         Ex: ${JOB_NAME}/${BUILD_NUMBER} should be specified for \
+                             andriod and for \
+                             kernel-hwpack ${KERNEL_NAME}/${KERNEL_JOB_NAME}")
+parser.add_argument("-n", "--build_num", dest="build_num", type=int,
+                    help="Specify the job build number for android builds only")
 
 
 acceptable_job_types = [
@@ -16,15 +27,30 @@ target_path = '/srv3/snapshots.linaro.org/www'
 class InvalidParametersException(Exception):
     pass
 
-
 class SnapshotsPublisher(object):
-    def run(self, arguments):
-        optparse...
-        if (arguments is None or len(arguments) < 3 or
-            arguments[0] not in acceptable_job_types):
+ 
+    def validate_args(self, args):
+        # Validate that all the required information is passed on the command line
+        if (args.job_type == None or  args.archive_info == None): 
+            parser.error("\nYou must specify job_type and archive_info")
             raise InvalidParametersException
+            return 1
+
+        if (args.job_type == "android" and args.build_num == None):
+            parser.error("You must specify build number")
+            raise InvalidParametersException
+            return 1
+
+        if (args.job_type not in acceptable_job_types):
+            parser.error("Invalid job type")
+            raise InvalidParametersException
+            return 1
+
+    def run(self, arguments):
+        pass
 
 
 if __name__ == '__main__':
     publisher = SnapshotsPublisher()
-    publisher.run(sys.argv)
+    args = parser.parse_args()
+    publisher.validate_args(args)
--- testing/test_publish_to_snapshots.py
+++ testing/test_publish_to_snapshots.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 from testtools import TestCase
 from testtools.matchers import Mismatch
 from scripts.publish_to_snapshots import (
@@ -12,26 +13,53 @@ class TestSnapshotsPublisher(TestCase):
     '''Tests for publishing files to the snapshots.l.o www are.'''
 
     def setUp(self):
+        self.parser =  argparse.ArgumentParser()
+        self.parser.add_argument("-j", "--job_type", dest="job_type")
+        self.parser.add_argument("-a", "--archive_info", dest="archive_info")
+        self.parser.add_argument("-n", "--build_num", dest="build_num", type=int)
         super(TestSnapshotsPublisher, self).setUp()
 
     def tearDown(self):
         super(TestSnapshotsPublisher, self).tearDown()
 
-    def test_run_invalid_parameters(self):
-        publisher = SnapshotsPublisher()
-        # There have to be at least 3 arguments passed to the run method.
-        self.assertRaises(InvalidParametersException,
-                          publisher.run, None)
-        self.assertRaises(InvalidParametersException,
-                          publisher.run, [1, 2])
+    def test_valid_job_values(self):
+        self.publisher = SnapshotsPublisher()
+        param = self.parser.parse_args(['-j', 'android', '-a', 'fun', '-n', '4'])
+        self.publisher.validate_args(param)
+   #     param = self.parser.parse_args(['-j', 'kernel-hwpack', '-a', 'fun', '-n', '4'])
+   #     self.publisher.validate_args(param)
 
-    def test_run_invalid_job_type(self):
-        publisher = SnapshotsPublisher()
-        self.assertRaises(InvalidParametersException,
-                          publisher.run, ["foo", "build-name", "4"])
+    def test_invalid_job_values(self):
+        self.publisher = SnapshotsPublisher()
+        param = self.parser.parse_args(['-a', 'fun', '-j', 'invalid_job', '-n', '4'])
+        try:
+            self.assertRaises(InvalidParametersException,
+                          self.publisher.validate_args, param)
+        except SystemExit, err:
+            print "DEBUG invalid job value"
 
-    def test_run_valid_job_types(self):
-        publisher = SnapshotsPublisher()
-        publisher.run(["android", "build-name", "4"])
-        publisher.run(["kernel-hwpack", "build-name", "4"])
+    def test_run_None_values(self):
+        self.publisher = SnapshotsPublisher()
+        try:
+            param = self.parser.parse_args(['-a', None , '-j', None, '-n', 0 ])
+            self.publisher.validate_args(param)
+        except SystemExit, err:
+            self.assertEqual(err.code, 2, "Expected result")
+
+    def test_run_invalid_option(self):
+        self.publisher = SnapshotsPublisher()
+        try:
+            param = self.parser.parse_args(['-x'])
+            self.publisher.validate_args(param)
+        except SystemExit, err:
+            print "DEBUG exception ArgumentParser", err
+            self.assertEqual(err.code, 2, "Expected result")
+
+    def test_run_invalid_type(self):
+        self.publisher = SnapshotsPublisher()
+        try:
+            param = self.parser.parse_args(['-n', "N"])
+            self.publisher.validate_args(param)
+        except argparse.ArgumentTypeError, details:
+            print "DEBUG exception argparse.ArgumentTypeError"
 
