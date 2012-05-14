970cd2da5874d6504ea1b98599f4d76edf41571b
Stevan Radakovic <stevan.radakovic@linaro.org>
2012-05-14 12:39:08 +0200

    Small updates from danilos code review.

--- testing/test_php_unit.py
+++ testing/test_php_unit.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python
-
 import os
+import tempfile
 import subprocess
-import xml.etree.ElementTree as etree
+import xml.etree.ElementTree
 
 from testtools import TestCase
 from testtools.matchers import Equals
+from testtools.matchers import AllMatch
 
 class PhpUnitTest(TestCase):
     '''Tests for executing the PHP Unit tests'''
 
     def setUp(self):
         super(PhpUnitTest, self).setUp()
-        self.xml_path = "testing/php_unit_test_result.xml"
+        self.xml_path = tempfile.mkstemp()[1]
         if subprocess.Popen(['phpunit', '--log-junit',
                              self.xml_path, 'testing/LicenseHelperTest'],
                 stdout=open('/dev/null', 'w'),
                 stderr=subprocess.STDOUT).wait():
             raise CommandNotFoundException("phpunit command not found. Please "
                 "install phpunit package and rerun tests.")
-        self.xml_data = etree.parse(self.xml_path)
+        self.xml_data = xml.etree.ElementTree.parse(self.xml_path)
 
     def tearDown(self):
         super(PhpUnitTest, self).tearDown()
@@ -27,7 +27,10 @@ class PhpUnitTest(TestCase):
             os.unlink(self.xml_path)
 
     def test_run_php_unit_tests(self):
-        self.assertThat(self.xml_data.getroot()[0].attrib['failures'],
-                        Equals("0"))
-        self.assertThat(self.xml_data.getroot()[0].attrib['errors'],
-                        Equals("0"))
+        self.assertThat(
+            [
+            self.xml_data.getroot()[0].attrib['failures'],
+            self.xml_data.getroot()[0].attrib['errors']
+            ],
+            AllMatch(Equals("0"))
+        )
