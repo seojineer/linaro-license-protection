b981ef1a8558bd0032b212c557206a2d037a56ea
Georgy Redkozubov <georgy.redkozubov@linaro.org>
2012-05-25 13:52:12 +0400

    Added 'Files-Pattern' field handling

--- licenses/BuildInfo.php
+++ licenses/BuildInfo.php
@@ -3,27 +3,67 @@
 class BuildInfo
 {
     private $text_array = array();
+    private $fields_defined = array("Format-Version", "Files-Pattern",
+        "Build-Name", "Theme", "License-Type", "OpenID-Launchpad-Teams",
+        "Collect-User-Data", "License-Text");
     private $multiline_vars = array("License-Text");
 
     public function readFile($fn)
     {
         $field = '';
+        $fp_read = 0;
         if (is_dir($fn) or !is_file($fn)) return false;
         $file = fopen($fn, "r") or exit("Unable to open file $fn!");
+        // Get the 'Format-Version' field.
+        $line = fgets($file);
+        $fields = explode(":", $line, "2");
+        $this->text_array[$fields[0]] = trim($fields[1]);
+        // Get the rest fileds.
         while(!feof($file)) {
+            if ($fp_read) {
+                print_r($fields);
+                $this->text_array[$fields[0]] = trim($fields[1]);
+                $fp_read = 0;
+            }
             $line = fgets($file);
             if (trim($line) == "")
                 continue;
             $fields = explode(":", $line, "2");
-            if (isset($fields[1])) {
-                $field = $fields[0];
-                $this->text_array[$field] = trim($fields[1]);
-            }
-            elseif (in_array($field, $this->multiline_vars)) {
+            if ($fields[0] == "Files-Pattern") {
+                $tmp_arr = array();
+                $fp = trim($fields[1]);
                 while(!feof($file)) {
                     $line = fgets($file);
-                    $this->text_array[$field] = $this->text_array[$field]."\n".rtrim($line);
+                    if (trim($line) == "")
+                        continue;
+                    $fields = explode(":", $line, "2");
+                    if (in_array($fields[0], $this->multiline_vars)) {
+                        $field = $fields[0];
+                        if (isset($fields[1]))
+                            $tmp_arr[$fields[0]] = trim($fields[1]);
+                        while(!feof($file)) {
+                            $line = fgets($file);
+                            if (trim($line) == "")
+                                continue;
+                            $fields = explode(":", $line, "2");
+                            if(in_array($fields[0], $this->fields_defined)) {
+                                if ($fields[0] == "Files-Pattern") {
+                                    fseek($file, -(strlen($line)), SEEK_CUR);
+                                    break 2;
+                                }
+                                break;
+                            }
+                            $tmp_arr[$field] = $tmp_arr[$field].
+                                "\n".rtrim($line);
+                        }
+
+                    }
+                    if (isset($fields[1])) {
+                        $tmp_arr[$fields[0]] = trim($fields[1]);
+                    }
                 }
+                $this->text_array[$fp] = $tmp_arr;
+                unset($tmp_arr);
             }
         }
         fclose($file);
