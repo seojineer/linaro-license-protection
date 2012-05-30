dc00772a2d655195d73019a3b85eeaf81537ed2d
Georgy Redkozubov <georgy.redkozubov@linaro.org>
2012-05-30 11:08:14 +0400

    Merged PHP unit tests into single test class

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,6 @@ def test_suite():
         'tests.test_click_through_license.TestLicense',
         'tests.test_publish_to_snapshots.TestSnapshotsPublisher',
         'tests.test_php_unit.PhpUnitTest',
-        'tests.test_build_info.BuildInfoTest',
         ]
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromNames(module_names)
