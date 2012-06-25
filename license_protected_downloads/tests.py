e2cf5240f501f7964c041406f1a908fb12aa78d3
Georgy Redkozubov <georgy.redkozubov@linaro.org>
2012-06-25 11:46:35 +0400

    Initial tests for models and views.

--- license_protected_downloads/tests.py
+++ license_protected_downloads/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+import unittest
+import hashlib
+from django.test import Client, TestCase
+from linaro_license_protection_2.models import License
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class LicenseTestCase(unittest.TestCase):
+    def setUp(self):
+        lic1_text = 'Samsung License'
+        lic2_text = 'Stericsson License'
+        digest1 = hashlib.md5(lic1_text).hexdigest()
+        digest2 = hashlib.md5(lic2_text).hexdigest()
+        self.lic1 = License.objects.create(digest=digest1, text=lic1_text,
+                theme='samsung')
+        self.lic2 = License.objects.create(digest=digest2, text=lic2_text,
+                theme='stericsson')
+
+    def test_add_license_to_database(self):
+        self.assertEquals(self.lic1.theme, 'samsung')
+        self.assertEquals(self.lic2.theme, 'stericsson')
+
+        lic1 = License.objects.get(pk=1)
+        self.assertEquals(lic1.theme, 'samsung')
+        self.assertEquals(lic1.text, 'Samsung License')
+        lic2 = License.objects.get(pk=2)
+        self.assertEquals(lic2.theme, 'stericsson')
+        self.assertEquals(lic2.text, 'Stericsson License')
+
+
+class ViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_license_directly(self):
+        response = self.client.get('/licenses/license.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Index of /')
+
+    def test_licensefile_directly_samsung(self):
+        response = self.client.get('/licenses/samsung.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Index of /')
+
