de40f2e957141e54ca73da47c3499ccb6acd7d2f
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2012-07-20 15:42:00 +0300

    Remove unused var and imports to fix pyflakes error.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,6 @@
 import unittest
-import tests.test_click_through_license
-import tests.test_publish_to_snapshots
 
 
 def test_suite():
-    modules = [
-        tests.test_click_through_license,
-        tests.test_publish_to_snapshots,
-        ]
     loader = unittest.TestLoader()
     return loader.discover('tests', top_level_dir='tests')
