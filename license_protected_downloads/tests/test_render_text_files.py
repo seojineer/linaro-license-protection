4866e6e3f0b48731f2cae1b2df0cda412d0e5f64
Stevan Radakovic <stevan.radakovic@linaro.org>
2012-10-11 11:48:13 +0200

    Add empty test file.

---
+++ license_protected_downloads/tests/test_render_text_files.py
@@ -0,0 +1,10 @@
+import os
+import unittest
+#from license_protected_downloads.render_text_files import RenderTextFiles
+
+THIS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+
+class RenderTextFilesTests(unittest.TestCase):
+    def setUp(self):
+        pass
