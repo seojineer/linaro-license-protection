a5b7b690aa8b4d88fcd49f893cc56635b090cc7b
Stevan Radakovic <stevan.radakovic@linaro.org>
2013-02-28 10:17:43 +0100

    Add test server helper from danilos branch.

--- license_protected_downloads/tests/helpers.py
+++ license_protected_downloads/tests/helpers.py
@@ -4,6 +4,10 @@
 import os
 import shutil
 import tempfile
+import threading
+import BaseHTTPServer
+import SocketServer
+import urllib2
 
 
 class temporary_directory(object):
@@ -39,3 +43,41 @@ class temporary_directory(object):
             target.write(data)
             target.close()
             return full_path
+
+
+class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path in self.urls:
+            self.send_response(200, "OK")
+            self.end_headers()
+            self.request.sendall(self.urls[self.path])
+        else:
+            self.send_error(404, 'URL %s not found' % self.path)
+            self.end_headers()
+
+class ThreadedHTTPServer(SocketServer.ThreadingMixIn,
+                         BaseHTTPServer.HTTPServer):
+    pass
+
+class TestHttpServer(object):
+    """Creates a context manager for a temporary directory."""
+
+    def __init__(self, urls):
+        self.handler = HttpHandler
+        self.handler.urls = urls
+
+    def __enter__(self):
+        self.server = ThreadedHTTPServer(
+            ("localhost", 0), self.handler)
+        self.ip, self.port = self.server.server_address
+        self.thread = threading.Thread(target=self.server.serve_forever)
+        self.thread.daemon = True
+        self.thread.start()
+        return self
+
+    def __exit__(self, *args):
+        self.server.shutdown()
+
+    @property
+    def base_url(self):
+        return "http://%s:%s" % (self.ip, self.port)
