ebdd7be876030119e03dc053edb98136c41d14ac
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2013-06-04 18:29:02 +0300

    First step toward normal logging setup.

--- settings.py
+++ settings.py
@@ -153,13 +153,22 @@ LOGGING = {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        # Root logger
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
         },
     }
 }
