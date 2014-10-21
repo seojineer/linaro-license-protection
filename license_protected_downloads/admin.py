091e3d9d44991e64ab690125ce7f0f449895174c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-10-21 18:01:15 +0300

    admin: Add tuned support for APIToken model.
    Change-Id: Ic4ee6e02bc84250d6128a41a698615deb748a2e2

--- license_protected_downloads/admin.py
+++ license_protected_downloads/admin.py
@@ -8,8 +8,12 @@ class APIKeyStoreAdmin(admin.ModelAdmin):
     fields = ('description', 'key', 'public')
     read_only_fields = ('last_used',)
 
+class APITokenAdmin(admin.ModelAdmin):
+    list_display = ('token', 'key', 'expires', 'ip')
+    read_only_fields = ('key',)
+
 
 admin.site.register(models.APIKeyStore, APIKeyStoreAdmin)
-admin.site.register(models.APIToken)
+admin.site.register(models.APIToken, APITokenAdmin)
 admin.site.register(models.APILog)
 admin.site.register(models.License)
