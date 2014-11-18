ea6a7ea95dad420a1e10b9552707eece7afb05be
Andy Doan <andy.doan@linaro.org>
2014-11-18 15:38:26 -0600

    auto populate api key in admin panel
    I decided not to do this for the APIToken since the aim of that is
    to be managed via the REST api which already auto-generates a
    random value
    
    Change-Id: Id1026e90b5f90d864696a71e4adec65aa851c121

--- license_protected_downloads/admin.py
+++ license_protected_downloads/admin.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib import admin
 
 from license_protected_downloads import models
@@ -7,6 +9,12 @@ class APIKeyStoreAdmin(admin.ModelAdmin):
     list_display = ('description', 'key', 'public', 'last_used')
     fields = ('description', 'key', 'public')
     read_only_fields = ('last_used',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(APIKeyStoreAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['key'].initial = str(uuid.uuid4())
+        return form
+
 
 class APITokenAdmin(admin.ModelAdmin):
     list_display = ('token', 'key', 'expires', 'ip')
