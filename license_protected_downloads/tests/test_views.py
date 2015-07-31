seojineer/linaro-license-protection

commit 65cd78e65b1bfb7836f170bc3b4af58a02a5b075

bug #1729: handle directory listings in a normal manner
The code base has always tried to handle a directory listing using the
same logic whether or not it included a trailing slash. This leads to
issues when people want to construct textile files that require relative
URLs.

There are 2 parts to this "fix":

1) start including the trailing slash when we generate our directory
listings.

2) perform a redirect if we are given a directory and don't have the
trailing slash.

Change-Id: I48c91ab9e9f1a72a3723fa4c84917db772b7215c

license_protected_downloads/views.py
@@ -154,6 +154,9 @@ def _check_build_info(request, build_info):
 
 
 def _handle_dir_list(request, artifact):
+    if request.path[-1] != '/':
+        return redirect(request.path + '/')
+
     # Generate a link to the parent directory (if one exists)
     url = artifact.url()
     if url != '/':

license_protected_downloads/tests/test_views.py
@@ -346,6 +346,13 @@ class ViewTests(BaseServeViewTest):
             r'<th></th><th>Name</th><th>Last modified</th>'
             '<th>Size</th><th style="display: None">License</th>')
 
+    def test_dir_redirect(self):
+        '''URLs to directs without trailing / should result in a redirect'''
+        url = 'http://testserver/~linaro-android'
+        response = self.client.get(url)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(url + '/', response['Location'])
+
     def test_not_found_file(self):
         target_file = "12qwaszx"
         url = urlparse.urljoin("http://testserver/", target_file)

license_protected_downloads/common.py
@@ -182,7 +182,10 @@ class Artifact(object):
                 url += '/'
         else:
             url = '/'
-        return url + self.file_name
+        url = url + self.file_name
+        if self.isdir() and url[-1] != '/':
+            url += '/'
+        return url
 
     def get_type(self):
         raise NotImplementedError()

commit a16b5d76e6a37f1ddc945c12389a4c29a5226bca

bug #1732: fix parent directory link
The previous commit, 65cd78e65b1bfb7836f170bc3b4af58a02a5b075, broke a
fragile assumption made on parent directory logic.

Ultimately we should probably think look around the code base and see if
we might be in need of some utility functions to help with this class of
problems.

Change-Id: Id2e22502fb6319596b20b9b2f693bdfa91f15f2b

license_protected_downloads/views.py
@@ -160,7 +160,13 @@ def _handle_dir_list(request, artifact):
     # Generate a link to the parent directory (if one exists)
     url = artifact.url()
     if url != '/':
+        if url[-1] == '/':
+            # we must remove trailing slash to find parent
+            url = url[:-1]
         up_dir = os.path.split(url)[0]
+        if up_dir[-1] != '/':
+            # now we need the trailing slash
+            up_dir += '/'
     else:
         up_dir = None
 

license_protected_downloads/tests/test_views.py
@@ -353,6 +353,17 @@ class ViewTests(BaseServeViewTest):
         self.assertEqual(302, response.status_code)
         self.assertEqual(url + '/', response['Location'])
 
+    def test_parent_dir(self):
+        '''Ensure the listing has the correct parent directory link'''
+        url = 'http://testserver/~linaro-android/'
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        self.assertIn('"/">Parent Directory', response.content)
+
+        url = 'http://testserver/~linaro-android/staging-panda/'
+        response = self.client.get(url, follow_redirect=True)
+        self.assertIn('"/~linaro-android/">Parent Directory', response.content)
+
     def test_not_found_file(self):
         target_file = "12qwaszx"
         url = urlparse.urljoin("http://testserver/", target_file)