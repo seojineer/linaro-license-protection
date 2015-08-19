seojineer/linaro-license-protection

commit 65cd78e65b1bfb7836f170bc3b4af58a02a5b075

bug #1729: handle directory listings in a normal manner
The code base has always tried to handle a directory listing using the
same logic whether or not it included a trailing slash. This leads to
issues when people want to construct textile files that require relative
URLs.

There are 2 parts to this "fix":

1) start including the trailing slash when we generate our directory
listings.

2) perform a redirect if we are given a directory and don't have the
trailing slash.

Change-Id: I48c91ab9e9f1a72a3723fa4c84917db772b7215c

license_protected_downloads/views.py
@@ -154,6 +154,9 @@ def _check_build_info(request, build_info):
 
 
 def _handle_dir_list(request, artifact):
+    if request.path[-1] != '/':
+        return redirect(request.path + '/')
+
     # Generate a link to the parent directory (if one exists)
     url = artifact.url()
     if url != '/':

license_protected_downloads/tests/test_views.py
@@ -346,6 +346,13 @@ class ViewTests(BaseServeViewTest):
             r'<th></th><th>Name</th><th>Last modified</th>'
             '<th>Size</th><th style="display: None">License</th>')
 
+    def test_dir_redirect(self):
+        '''URLs to directs without trailing / should result in a redirect'''
+        url = 'http://testserver/~linaro-android'
+        response = self.client.get(url)
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(url + '/', response['Location'])
+
     def test_not_found_file(self):
         target_file = "12qwaszx"
         url = urlparse.urljoin("http://testserver/", target_file)

license_protected_downloads/common.py
@@ -182,7 +182,10 @@ class Artifact(object):
                 url += '/'
         else:
             url = '/'
-        return url + self.file_name
+        url = url + self.file_name
+        if self.isdir() and url[-1] != '/':
+            url += '/'
+        return url
 
     def get_type(self):
         raise NotImplementedError()

commit 57c8878f3fefc5d8d483b19d0a04e1547958ef23

regression: fix accept license redirection
Normal behavior of l-l-p when clicking a protected file is:

 1) go to license accept
 2) accept
 3) get dir-listing page with "refresh" header pointing at file
 4) page will refresh to automatically download file

Commit:
 65cd78e65b1bfb7836f170bc3b4af58a02a5b075

broke this function. Step 3 just gave the user a standard directory
listing and they had to click the file to download.

Additionally, the unit test failed to test item #3. This fixes the
issue, and fixes the test.

Change-Id: I1aa223766148b132919c5bf383589911f8363cad

license_protected_downloads/views.py
@@ -154,7 +154,10 @@ def _check_build_info(request, build_info):
 
 
 def _handle_dir_list(request, artifact):
-    if request.path[-1] != '/':
+    download = None
+    if 'dl' in request.GET:
+        download = request.GET['dl']
+    elif request.path[-1] != '/':
         return redirect(request.path + '/')
 
     # Generate a link to the parent directory (if one exists)
@@ -172,10 +175,6 @@ def _handle_dir_list(request, artifact):
 
     # must come before call to find_and_render to optimize s3
     dirlist = dir_list(artifact)
-
-    download = None
-    if 'dl' in request.GET:
-        download = request.GET['dl']
     rendered_files = RenderTextFiles.find_and_render(artifact)
     ann = artifact.get_annotated_manifest()
     if ann:

license_protected_downloads/tests/test_views.py
@@ -325,10 +325,15 @@ class ViewTests(BaseServeViewTest):
         # cookies from the current session, and re-introduce them.
         client = Client()
         client.cookies[accept_cookie_name] = accept_cookie_name
-        response = client.get(url)
+        response = client.get(response['Location'])
 
-        # If we have access to the file, we will get an X-Sendfile response
+        # If we have access to the file, we get a page with "refresh" directive
         self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '<meta http-equiv="REFRESH" content="0;url=', response.content)
+
+        # now download the file
+        response = client.get(url)
         file_path = os.path.join(TESTSERVER_ROOT, target_file)
         self.assertEqual(response['X-Sendfile'], file_path)
 