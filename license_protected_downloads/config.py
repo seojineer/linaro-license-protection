c7f2c8e8c12f3398002b1acdc42cdaf4c4656ac8
Fathi Boudra <fathi.boudra@linaro.org>
2015-12-08 12:48:07 +0200

    config: whitelist TCWG LAVA micro instance
    Change-Id: I5f09ed9a9cd29cd8f5cd85176c141ef36ef6e610
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>
    Reviewed-on: https://review.linaro.org/9503

--- license_protected_downloads/config.py
+++ license_protected_downloads/config.py
@@ -3,6 +3,7 @@
 # Let internal hosts through always.
 INTERNAL_HOSTS = (
     '54.225.81.132',  # android-build.linaro.org
+    '81.128.185.42',  # tcwg.validation.linaro.org
     '81.128.185.50',  # lab.validation.linaro.org
     '81.128.185.52',  # lng.validation.linaro.org
     '188.40.92.79',   # ci.linaro.org (slave x86-64-07)
