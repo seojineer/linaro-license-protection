16b108d161c7168868dfd676320820d397ea90be
Benjamin Copeland <ben.copeland@linaro.org>
2018-04-30 13:08:01 +0100

    Whitelist: Update whitelist IPS
    Update whitelist IPS to the current slave list
    
    Change-Id: I716c25957bbbdf0591061c1e224b9a8a69ac5458
    Reviewed-on: https://review.linaro.org/25054
    Reviewed-by: Vishal Bhoj <vishal.bhoj@linaro.org>
    Reviewed-by: Benjamin Copeland <ben.copeland@linaro.org>

--- license_protected_downloads/config.py
+++ license_protected_downloads/config.py
@@ -2,30 +2,35 @@
 
 # Let internal hosts through always.
 INTERNAL_HOSTS = (
-    '54.225.81.132',  # android-build.linaro.org
-    '82.71.243.201',  # validation.linaro.org
+    '82.71.243.201',   # validation.linaro.org
     '82.71.243.203',
     '51.148.40.1',
     '51.148.40.7',
-    '81.128.185.42',  # tcwg.validation.linaro.org
-    '81.128.185.50',  # lab.validation.linaro.org
-    '81.128.185.52',  # lng.validation.linaro.org
-    '188.40.92.79',   # ci.linaro.org (slave x86-64-07)
-    '188.40.49.144',  # ci.linaro.org (slave x86-64-08)
-    '148.251.184.94', # ci.linaro.org (slave x86-64-09)
-    '138.201.52.83',  # ci.linaro.org (slave x86-64-10)
-    '88.99.28.12',    # ci.linaro.org (slave x86-64-11)
-    '88.99.28.38',    # ci.linaro.org (slave x86-64-12)
-    '144.76.6.139',   # ci.linaro.org (slave aosp-x86-64-07)
-    '188.40.51.209',  # ci.linaro.org (slave aosp-x86-64-08)
-    '213.133.116.86', # ci.linaro.org (slave aosp-x86-64-09)
-    '78.46.190.194',  # ci.linaro.org (slave aosp-x86-64-10)
-    '217.140.96.140', # cambridge.arm.com
-    '73.14.250.225', # Tyler's lab - nwdrone.com
-    '73.14.250.226', #
-    '73.14.250.227', #
-    '73.14.250.228', #
-    '73.14.250.229', #
+    '81.128.185.42',   # tcwg.validation.linaro.org
+    '81.128.185.50',   # lab.validation.linaro.org
+    '81.128.185.52',   # lng.validation.linaro.org
+    '144.76.6.139',    # aosp-x86_64-07
+    '188.40.51.209',   # aosp-x86_64-08
+    '213.133.116.86',  # aosp-x86_64-09 (ART)
+    '78.46.190.194',   # aosp-x86_64-10 (ART)
+    '148.251.140.195', # lhg-build-01
+    '188.40.92.79',    # x86_64-07
+    '188.40.49.144',   # x86_64-08
+    '148.251.184.94',  # x86_64-09
+    '138.201.52.83',   # x86_64-10
+    '88.99.28.12',     # x86_64-11 (RPB)
+    '88.99.28.38',     # x86_64-12 (RPB)
+    '88.99.59.232',    # x86_64-13 (LITE)
+    '88.99.149.141',   # x86_64-14
+    '51.15.185.142',   # lkft-build-01
+    '62.210.248.69',   # oe-x86_64-01
+    '62.210.249.170',  # oe-x86_64-02
+    '217.140.96.140',  # cambridge.arm.com
+    '73.14.250.225',   # Tyler's lab - nwdrone.com
+    '73.14.250.226',   #
+    '73.14.250.227',   #
+    '73.14.250.228',   #
+    '73.14.250.229',   #
 )
 
 WHITELIST = (
